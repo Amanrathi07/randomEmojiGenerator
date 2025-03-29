235275b67d26fdaa9e6c719ac3a550b5618ec6d3
Amanrathi07 <amanrathi4321@gmail.com>
2025-03-29 11:45:34 +0530

    complet

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
-let click = document.getElementById('click');
-let disc = document.getElementById('disc');
-click.addEventListener('click',(e)=>{
-    
-   let data =  callApi();      
+let click = document.getElementById("click");
+let disc = document.getElementById("disc");
 
-    data.then((res)=>{
-        let randomNum = Math.floor(Math.random()*res.length);
-        
-        let discpiration = res[randomNum].unicodeName ;
+click.addEventListener("click", (e) => {
+  let data = callApi();
 
-        let arr = discpiration.split('.');
-        console.log();
-        
-        
-        e.target.innerText=res[randomNum].character;
-        disc.innerHTML= arr[1].substring(1) ;
-        })
-})
+  data.then((res) => {
+    let randomNum = Math.floor(Math.random() * res.length);
 
+    let discpiration = res[randomNum].unicodeName;
 
+    let arr = discpiration.split(".");
+    console.log();
+
+    e.target.innerText = res[randomNum].character ;
+    disc.innerHTML = arr[1].substring(1);
+  });
+});
 
 async function callApi() {
-    let result = await fetch("https://emoji-api.com/emojis?access_key=518fc60777fcd7465d55d2f008676e273b708090");
-    let data = await result.json();
-    
-    return  data  
-    
-    
+  let result = await fetch(
+    "https://emoji-api.com/emojis?access_key=518fc60777fcd7465d55d2f008676e273b708090"
+  );
+  let data = await result.json();
+
+  return data;
 }
-
-
